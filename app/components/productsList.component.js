4ce8146f7b9c5f71fb889477cf8899b07a32b093
Mateusz Kocz <mateusz@kocz.pl>
2017-02-03 20:36:44 +0100

    feat(components): add info when there’s no products

--- app/components/productsList.component.js
+++ app/components/productsList.component.js
@@ -1,5 +1,6 @@
 const template = `
-<product-item ng-repeat="product in $ctrl.products" item="product"></product-item>`
+<product-item ng-repeat="product in $ctrl.products" item="product"></product-item>
+<p ng-if="!$ctrl.products.length">No items yet. Subscribe to our fantastic newsletter!</p>`
 
 export const name = 'productsList'
 export const properties = {
