9eebf085ad04ac6e754c208347fa1ba52a5783da
Mateusz Kocz <mateusz@kocz.pl>
2017-02-03 20:13:06 +0100

    feat(components): add transclusions

--- app/components/components.module.js
+++ app/components/components.module.js
@@ -1,5 +1,6 @@
 import * as pageHeaderComponent from './pageHeader.component'
 import * as pageFooterComponent from './pageFooter.component'
+import * as pageContentComponent from './pageContent.component'
 import * as shopLogoComponent from './shopLogo.component'
 import * as shopCartComponent from './shopCart.component'
 import * as searchBarComponent from './searchBar.component'
@@ -8,6 +9,7 @@ import * as productsListComponent from './productsList.component'
 export const moduleName = 'components'
 angular.module(moduleName, [])
     .component(pageHeaderComponent.name, pageHeaderComponent.properties)
+    .component(pageContentComponent.name, pageContentComponent.properties)
     .component(pageFooterComponent.name, pageFooterComponent.properties)
     .component(shopLogoComponent.name, shopLogoComponent.properties)
     .component(shopCartComponent.name, shopCartComponent.properties)
