c15089b4eb96d076fd9bc1a04a5c4212cd8af451
Mateusz Kocz <mateusz@kocz.pl>
2017-02-04 13:36:56 +0100

    feat(components): add code to the search component

--- app/components/productsList.component.js
+++ app/components/productsList.component.js
@@ -1,5 +1,5 @@
 const template = `
-<product-item ng-repeat="product in $ctrl.products" item="product" on-add-to-cart="$ctrl.addToCart(product, amount)"></product-item>
+<product-item ng-repeat="product in $ctrl.products | filter:{name: $ctrl.search} track by product.id" item="product" on-add-to-cart="$ctrl.addToCart(product, amount)"></product-item>
 <p ng-if="!$ctrl.products.length">No items yet. Subscribe to our fantastic newsletter!</p>`
 
 class ProductsListController {
@@ -13,7 +13,8 @@ export const properties = {
   template,
   bindings: {
     products: '<items',
-    requestAddToCart: '&onAddToCart'
+    requestAddToCart: '&onAddToCart',
+    search: '<?'
   },
   controller: ProductsListController
 }
--- app/components/searchBar.component.js
+++ app/components/searchBar.component.js
@@ -1,4 +1,26 @@
+const template = `
+<form name="searchForm" ng-submit="$ctrl.submitSearch()">
+  <input type="text" ng-model="$ctrl.searchTerm">
+  <button>Search</button>
+  <button ng-click="$ctrl.clearSearch()">Clear</button>
+</form>`
+
+class SearchBarController {
+  submitSearch() {
+    this.search({term: this.searchTerm})
+  }
+
+  clearSearch() {
+    this.searchTerm = ''
+    this.submitSearch()
+  }
+}
+
 export const name = 'searchBar'
 export const properties = {
-  template: `<div>Search bar</div>`
+  template,
+  controller: SearchBarController,
+  bindings: {
+    search: '&onSearch'
+  }
 }
